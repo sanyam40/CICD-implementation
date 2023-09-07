6f0645c99652b6af7e887a492e02356295057f7d
sanyam <sanyamnarang40@gmail.com>
2023-09-07 13:58:39 +0530

    code added

---
+++ src/main/java/com/boot/PaymentIntegrationApplication.java
@@ -0,0 +1,19 @@
+package com.boot;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class PaymentIntegrationApplication{
+	
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentIntegrationApplication.class, args);
+	}
+	
+	  
+
+}
